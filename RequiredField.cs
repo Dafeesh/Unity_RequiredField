14d4b9a1b847ea5013389e53d15bef75427d80d1
Dafeesh <blake.scherschel@gmail.com>
2015-12-03 07:11:21 -0500

    Added RF file.

---
+++ RequiredField.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.Callbacks;
+#endif
+
+[InitializeOnLoad, AttributeUsage(AttributeTargets.Field)]
+public class RequiredField : Attribute
+{
+#if UNITY_EDITOR
+    static RequiredField()
+    {
+        EditorApplication.playmodeStateChanged += OnPlaymodeStateChanged;
+    }
+
+    static void OnPlaymodeStateChanged()
+    {
+        if (EditorApplication.isPlaying)
+            CheckRequiredFields();
+    }
+
+    //[DidReloadScripts] //Optional if you want to check whenever scripts compile.
+    static void CheckRequiredFields()
+    {
+        MonoBehaviour firstMissingReference = null;
+        MonoBehaviour[] allComponents = GameObject.FindObjectsOfType<MonoBehaviour>();
+        foreach (var obj in allComponents)
+        {
+            IEnumerable<FieldInfo> fieldInfos = obj.GetType()
+                .GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                .Where((f) => f.GetCustomAttributes(typeof(RequiredField), true)
+                    .Any((o) => o is RequiredField));
+
+            foreach (FieldInfo fieldInfo in fieldInfos)
+            {
+                object value = fieldInfo.GetValue(obj);
+                if (value == null || value.Equals(null))
+                {
+                    firstMissingReference = obj;
+                    Debug.LogError(
+                        "Missing required field value! " +
+                        "'" + obj.name + "' (" + obj.GetType().ToString() + ")", obj);
+                }
+            }
+        }
+
+        if (firstMissingReference != null && EditorApplication.isPlaying)
+            firstMissingReference.StartCoroutine(StopEditorAtEndOfFrame());
+    }
+
+    static IEnumerator StopEditorAtEndOfFrame()
+    {
+        yield return new WaitForEndOfFrame();
+        EditorApplication.isPlaying = false;
+    }
+#endif
+}
